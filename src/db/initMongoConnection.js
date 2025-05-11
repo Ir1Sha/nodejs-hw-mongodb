a1114bc93c2090fd3151fa63f8040491bc9acb76
ginny <ir.gonccharova.up@gmail.com>
2025-05-11 22:08:03 +0300

    Fix: update MongoDB connection to use separate env vars

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 
 export async function initMongoConnection() {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    const user = process.env.MONGODB_USER;
+    const password = process.env.MONGODB_PASSWORD;
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
+
+    const uri = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(uri);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Mongo connection error:', error.message);
