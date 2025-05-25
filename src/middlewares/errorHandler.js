4f67fb743bd49aa5386026cc7432bc717f8e454e
ginny <ir.gonccharova.up@gmail.com>
2025-05-25 16:16:10 +0300

    validateBody upd

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,9 +2,14 @@ export function errorHandler(err, req, res, next) {
   const status = err.status || 500;
   const message = err.message || 'Something went wrong';
 
-  res.status(status).json({
+  const responseBody = {
     status,
     message,
-    data: message,
-  });
+  };
+
+  if (err.errors) {
+    responseBody.errors = err.errors;
+  }
+
+  res.status(status).json(responseBody);
 }
